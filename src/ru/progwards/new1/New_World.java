5d803fb4c7c3b5e0b579983a1b4e396877febcd3
Боря <Batishta_83@mail.ru>
2019-11-13 13:16:22 +0300

    HelloWorld_New

---
+++ src/ru/progwards/new1/New_World.java
@@ -0,0 +1,20 @@
+package ru.progwards.new1;
+
+public class New_World {
+    static void println(String str){
+        System.out.println(str);
+
+    }
+    static int addition(int x, int y){
+        return x+y;
+    }
+    public static void main(String[] args){
+        println("Привет мир!");
+        println("Мирб ау ?!");
+        System.out.println("Язнаю, что 5 + 7");
+        System.out.println(addition(5,7));
+
+    }
+
+
+}
