8c0cea81050c8c71fa5d9a24af35f404e95a4a5a
Боря <Batishta_83@mail.ru>
2019-11-13 15:51:42 +0300

    Task1

---
+++ src/ru/progwards/new1/Task1.java
@@ -0,0 +1,10 @@
+package ru.progwards.new1;
+
+public class Task1 {
+    public static void main(String[] args){
+        String s= "Хорошо идут дела ";
+        String a= "Изучаю Java я!";
+        System.out.println(s+a);
+        System.out.println(a+s);
+    }
+}
